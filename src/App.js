85b11f22546b4e3086df5086a3c019a114794bf3
unknown <gabrielayoku@gmail.com>
2024-03-19 08:02:41 +0100

    updating images on the home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
 
 function App() {
+  const screen = window.screen.availWidth;
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
+        {screen > 900 && <Header />}
         <Routes>
-          <Route exact path='/' element={<Home/>} />
+          <Route exact path="/" element={<Home />} />
         </Routes>
-      <Footer/>
+        {screen > 900 && <Footer />}
       </BrowserRouter>
     </div>
   );
